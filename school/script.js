467b4879a38e43a1295cf84adac1914ae6355dc9
Dhyanam Pathak <101004943+Dhyanam04@users.noreply.github.com>
2023-09-30 16:16:41 +0530

    Add files via upload

---
+++ school/script.js
@@ -0,0 +1,7 @@
+function showContent(contentId) {
+    const containers = document.querySelectorAll('.container');
+    containers.forEach(container => {
+        container.classList.remove('active-container');
+    });
+    document.getElementById(contentId).classList.add('active-container');
+}
